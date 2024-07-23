1aa8d506091cc37c86898bcc13367fe95bc7d295
Denis.Zateev <zateevdenis@gmail.com>
2024-07-23 15:51:59 +0300

    рефакторинг

--- app/src/main/java/ru/icecreamru/lifexp/data/local/AppDatabase.kt
+++ app/src/main/java/ru/icecreamru/lifexp/data/local/AppDatabase.kt
@@ -30,7 +30,7 @@ abstract class AppDatabase : RoomDatabase() {
 
         private fun buildDatabase(context: Context) =
             Room.databaseBuilder(context, AppDatabase::class.java, "lifexp_database")
-                .addCallback(object : RoomDatabase.Callback() {
+                .addCallback(object : Callback() {
                     override fun onCreate(db: SupportSQLiteDatabase) {
                         super.onCreate(db)
                         CoroutineScope(Dispatchers.IO).launch {
